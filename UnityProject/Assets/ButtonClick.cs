6ee60595972986ae382e4d733b841b9b76be3a9c
Vincent Zhang <zhangwei983@hotmail.com>
2023-02-23 12:17:19 +0800

    Add the function to register a protocol handler.

--- UnityProject/Assets/ButtonClick.cs
+++ UnityProject/Assets/ButtonClick.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+#if PLATFORM_STANDALONE_WIN
+using Microsoft.Win32;
+#endif
 
 public class ButtonClick : MonoBehaviour
 {
     public string testTarget = @"C:\test.html";
+    public string myRootKey = "vincenttest";
+    public string applicationPath; // = @"C:\Projects\vs\TestBrowser\TestBrowser\bin\Debug\net6.0-windows\TestBrowser.exe";
 
     public void BrowserButtonClick()
     {
@@ -11,7 +16,33 @@ public class ButtonClick : MonoBehaviour
 
     public void RegisterButtonClick()
     {
-        // TODO.
-        Debug.Log("Register Protocol handler");
+        Debug.Log(Application.dataPath);
+
+#if PLATFORM_STANDALONE_WIN
+        // This is just used to demo how we register a protocol handler.
+        // You should rely on the installer to register in real case.
+        var regCurrentUser = Registry.CurrentUser;
+        var regClassesRoot = regCurrentUser.OpenSubKey("SOFTWARE", true)?.OpenSubKey("Classes", true);
+        if (regClassesRoot == null)
+            return;
+
+        var regMyRoot = regClassesRoot.OpenSubKey(myRootKey, true);
+        if (regMyRoot != null)
+        {
+            regClassesRoot.DeleteSubKeyTree(myRootKey);
+        }
+
+        var newRegMyRoot = regClassesRoot.CreateSubKey(myRootKey, true);
+        newRegMyRoot.SetValue("", "URL:" + myRootKey);
+        newRegMyRoot.SetValue("URL Protocol", "");
+
+        var regIcon = newRegMyRoot.CreateSubKey("DefaultIcon");
+        regIcon.SetValue("", "OpenBrowser.exe");
+
+        var regCommand = newRegMyRoot.CreateSubKey("shell").CreateSubKey("open").CreateSubKey("command");
+        regCommand.SetValue("", "\"" + applicationPath + "\" \"%1\"");
+
+        return;
+#endif
     }
 }
